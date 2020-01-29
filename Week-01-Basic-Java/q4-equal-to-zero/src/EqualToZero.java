1388982a3c784b05573a2b97160b9d29ce921c6a
MargotGuo <gyqgdn@126.com>
2020-01-29 14:40:21 +0800

    [Guo Yuanqi]{feat}: operations

--- Week-01-Basic-Java/q4-equal-to-zero/src/EqualToZero.java
+++ Week-01-Basic-Java/q4-equal-to-zero/src/EqualToZero.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EqualToZero {
+
+  public static boolean equalToZero(double number) {
+    return Math.abs(number) < 1e-15;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    double input = in.nextDouble();
+    System.out.println(equalToZero(input));
+  }
 }
