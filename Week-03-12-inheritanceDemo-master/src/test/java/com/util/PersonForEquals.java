49c2c5c3cdd8fff4fa3f837218691195277e553d
MargotGuo <gyqgdn@126.com>
2020-02-16 23:44:24 +0800

    [Guo Yuanqi]{feat}: practice for inheritence

---
+++ Week-03-12-inheritanceDemo-master/src/test/java/com/util/PersonForEquals.java
@@ -0,0 +1,56 @@
+package com.util;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public class PersonForEquals {
+
+  private final String name;
+  private final short yearOfBirth;
+
+  public PersonForEquals(String name, short yearOfBirth) {
+    if (name == null) {
+      throw new IllegalArgumentException("name is mandatory.");
+    }
+
+    if (yearOfBirth <= 1900 || yearOfBirth >= 2019) {
+      throw new IllegalArgumentException("year of birth is out of range.");
+    }
+
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public short getYearOfBirth() {
+    return yearOfBirth;
+  }
+
+  @SuppressWarnings("Contract")
+  @Override
+  public boolean equals(Object obj) {
+    // TODO: please modify the following code to pass the test
+    // <--start
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    PersonForEquals that = (PersonForEquals) obj;
+    return yearOfBirth == that.yearOfBirth &&
+        name.equals(that.name);
+    // --end-->
+  }
+
+  @Override
+  public int hashCode() {
+    // TODO: please modify the following code to pass the test
+    // <--start
+    return Objects.hash(name, yearOfBirth);
+//    throw new NotImplementedException();
+    // --end-->
+  }
+}
