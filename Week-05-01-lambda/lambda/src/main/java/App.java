493a1a15d3e485fe4ff73c0769a5d4d1fb5583df
MargotGuo <gyqgdn@126.com>
2020-03-03 14:37:48 +0800

    [Guo Yuanqi]{refactor}: for lambda

--- Week-05-01-lambda/lambda/src/main/java/App.java
+++ Week-05-01-lambda/lambda/src/main/java/App.java
@@ -4,33 +4,29 @@ import java.util.function.Function;
 public class App {
   public static void main(String[] args) {
     String originalString = "test";
-    invokeStringOperator(originalString);
-    addStarByConsumer(originalString);
+    invokeStringOperator(originalString, str -> System.out.println("*******" + str + "*******"));
+    addStarByConsumer(originalString, str -> System.out.println("*******" + str + "*******"));
 
-    int number = 3;
-    int result1 = invokeNumberOperator(number);
-    int result2 = getSquareByFunction(number);
+    int inputNumber = 3;
+    int result1 = invokeNumberOperator(inputNumber, number -> number * number);
+    int result2 = getSquareByFunction(inputNumber, number -> number * number);
     System.out.println(result1);
     System.out.println(result2);
   }
 
-  private static void invokeStringOperator(String inputString) {
-    StringOperator<String> stringOperator = str -> System.out.println("*******" + str + "*******");
+  private static void invokeStringOperator(String inputString, StringOperator<String> stringOperator) {
     stringOperator.addStar(inputString);
   }
 
-  private static void addStarByConsumer(String inputString) {
-    Consumer<String> consumer = str -> System.out.println("*******" + str + "*******");
+  private static void addStarByConsumer(String inputString, Consumer<String> consumer) {
     consumer.accept(inputString);
   }
 
-  private static int invokeNumberOperator(int inputNumber) {
-    NumberOperator<Integer> numberOperator = number -> number * number;
+  private static int invokeNumberOperator(int inputNumber, NumberOperator<Integer> numberOperator) {
     return numberOperator.getSquare(inputNumber);
   }
 
-  private static int getSquareByFunction(int inputNumber) {
-    Function<Integer, Integer> function = number -> number * number;
+  private static int getSquareByFunction(int inputNumber, Function<Integer, Integer> function) {
     return function.apply(inputNumber);
   }
 }
