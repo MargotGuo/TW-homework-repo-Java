21625b4906f019e3d5c192bb5c616d3e1a8e6c6b
MargotGuo <gyqgdn@126.com>
2020-03-20 20:44:47 +0800

    [Guo Yuanqi]{refactor}: for wordProcessor

--- Week-06-04-quiz-wordProcessor/src/test/java/quiz/wordProcessor/TextProcessor.java
+++ Week-06-04-quiz-wordProcessor/src/test/java/quiz/wordProcessor/TextProcessor.java
@@ -2,7 +2,6 @@ package quiz.wordProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -31,26 +30,27 @@ class TextProcessor {
   String process(String text) {
     // TODO: Please implement the method to pass all the test
     // <--start
+    if (text == null) {
+      throw new IllegalArgumentException();
+    }
     AtomicInteger textLength = new AtomicInteger();
-    return getWordList(text).stream()
+    return splitTextToList(text).stream()
         .map(word -> String.format("%s(%s);", word, getLineDetail(textLength.addAndGet(word.length()), word.length())))
         .collect(Collectors.joining(""));
     // --end-->
   }
 
-  private List<String> getWordList(String text) {
-    return Optional.ofNullable(text).map(sentence -> {
-      List<String> wordList = new ArrayList<>();
-      int startFlag = 0;
-      for (int i = 1; i < sentence.length(); i++) {
-        if (settings.isWhitespace(sentence.charAt(i - 1)) != settings.isWhitespace(sentence.charAt(i))) {
-          wordList.add(sentence.substring(startFlag, i));
-          startFlag = i;
-        }
+  private List<String> splitTextToList(String text) {
+    List<String> wordList = new ArrayList<>();
+    int startFlag = 0;
+    for (int i = 1; i < text.length(); i++) {
+      if (settings.isWhitespace(text.charAt(i - 1)) != settings.isWhitespace(text.charAt(i))) {
+        wordList.add(text.substring(startFlag, i));
+        startFlag = i;
       }
-      wordList.add(sentence.substring(startFlag));
-      return wordList;
-    }).orElseThrow(IllegalArgumentException::new);
+    }
+    wordList.add(text.substring(startFlag));
+    return wordList;
   }
 
   private String getLineDetail(int sentenceLength, int wordLength) {
